df65982f026df83e1264a4dd09b617922de76ab2
Evgen Pliska <shenjapliska@gmail.com>
2017-05-12 00:38:55 +0300

    changes

--- API_Pro_GUI_Tests/API_Pro_GUI_Tests/Tests/TestBase.cs
+++ API_Pro_GUI_Tests/API_Pro_GUI_Tests/Tests/TestBase.cs
@@ -8,6 +8,7 @@ using NUnit.Framework;
 using NUnit.Framework.Internal;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
 
 namespace API_Pro_GUI_Tests.Tests
 {
